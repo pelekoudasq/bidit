9f52323beda742db8fb3c38facb7bdcaaeea4492
Ioannis Pelekoudas <peleioannis@gmail.com>
2019-04-25 03:12:00 +0300

    server port - expressjs

---
+++ server.js
@@ -0,0 +1,31 @@
+//bring express and body-parser module from node_modules
+const express = require('express');
+const app = express();
+//const bodyParser = require('body-parser');
+//system module
+//const path = require('path');
+//const http = require('http');
+//const cors = require('cors');
+
+//const jwt = require('./server/routes/jwt');
+
+//Set Port
+const port = 3000;
+
+//API file for interacting with mongoDB
+//const api = require('./server/routes/api');
+
+
+//Body Parsers middle ware
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(cors());
+
+//app.use(jwt());
+
+//API location
+//app.use('/api', api.router);
+
+app.listen(port, function(){
+    console.log('Server started on port '+port);
+})
