8917d6fc16f9cc431b4392b36da9166e416f1430
SofiaKstk <sofkontis@gmail.com>
2019-08-09 15:22:22 +0300

    several tweaks
    scroll to top on route, add multiple categ to new auct, profile on card,etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router';
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 import { AuthenticationService } from './services/authentication.service';
 import { User } from './models/user';
@@ -17,7 +19,9 @@ export class AppComponent {
 
 	constructor(
 		private authenticationService: AuthenticationService,
-		private router: Router,) {
+		private router: Router,
+		@Inject(PLATFORM_ID) private platformId: Object,
+		@Inject(APP_ID) private appId: string) {
 		
 	}
 	
@@ -32,5 +36,19 @@ export class AppComponent {
 	logout() {
 		this.authenticationService.logout();
 		this.router.navigate(['/login']);
+	}
+
+	onActivate(event: any) {
+		if (isPlatformBrowser(this.platformId)) {
+		  	let scrollToTop = window.setInterval(() => {
+				let pos = window.pageYOffset;
+				if (pos > 0) {
+					// window.scrollTo(0, pos - 20); // how far to scroll on each step
+					window.scrollTo(0, 0); // how far to scroll on each step
+				} else {
+					window.clearInterval(scrollToTop);
+				}
+		 	}, 16);
+		}
 	}
 }
