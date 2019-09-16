9158a574c1af8ba0636987864c2d03d88479f68b
SofiaKstk <sofkontis@gmail.com>
2019-09-16 19:27:13 +0300

    xml file download

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { saveAs } from 'file-saver';
 
 import { AlertService } from '../../services/alert.service';
 import { AuthenticationService } from '../../services/authentication.service';
@@ -19,15 +20,13 @@ export class AdminComponent implements OnInit {
 
 	users: User[] = [];
 	auctions: Auction[] = [];
-	xmlAuction: any;
-	auctionJsonUri = [];
-	auctionXmlUri = [];
 	approvedUsers: User[] = [];
 	disapprovedUsers: User[] = [];
 	currentUser: User;
 	loggedin: boolean;
 	loading: boolean = false;
 	loading2: boolean = false;
+	file: any = null;
 
 	constructor(
 		private sanitizer: DomSanitizer,
@@ -50,40 +49,6 @@ export class AdminComponent implements OnInit {
 		});
 		this.dataService.getAuctions().pipe(first()).subscribe(auctions =>{
 			this.auctions = auctions;
-			for (var i = 0; i < this.auctions.length; i++) {
-				// var auct = this.auctions[i];				
-				// // XML
-				// var XMLWriter = require('xml-writer');
-				// var xw = new XMLWriter(true);
-				// xw.startElement('Item');
-				// xw.writeAttribute('ItemID', auct._id);
-				// xw.writeElement('Name', auct.name);
-				// auct.categories.forEach(cat => {
-				// 	xw.writeElement('Category', cat)
-				// });
-				// xw.writeElement('Currently', "$"+auct.currently);
-				// if(auct.buy_price)
-				// 	xw.writeElement('Buy_Price', "$"+auct.buy_price);
-				// xw.writeElement('First_Bid', "$"+auct.first_bid);
-				// xw.writeElement('Number_of_Bids', auct.number_of_bids);
-				// // xw.startElement('Bids');
-				// // xw.endElement();
-				// xw.writeElement('Location', auct.location.name).writeAttribute('Latitude', auct.location.latitude).writeAttribute('Longtitude', auct.location.longitude);
-				// xw.writeElement('Country', auct.country);
-				// if(auct.started){
-				// 	xw.writeElement('Started', auct.startingDate);			
-				// 	xw.writeElement('Ends', auct.endingDate);
-				// }
-				// // xw.writeElement('Seller');
-				// xw.writeElement('Description', auct.description);				
-				// this.auctionXmlUri.push(this.sanitizer.bypassSecurityTrustUrl("data:application/xml;charset=UTF-8," + encodeURIComponent(xw)));
-				// JSON
-				// var convert = require('xml-js');
-				// var options = {compact: true, ignoreComment: true, spaces: 4};
-				// var theJSON = convert.xml2json(xw, options);
-				// this.auctionJsonUri.push(this.sanitizer.bypassSecurityTrustUrl("data:application/json;charset=UTF-8," + encodeURIComponent(theJSON)));
-				
-			}
 			this.loading2 = true;
 		});
 	}
@@ -97,7 +62,6 @@ export class AdminComponent implements OnInit {
                     this.users = [];
                     this.approvedUsers = [];
                     this.disapprovedUsers = [];
-                    this.auctionJsonUri = [];
                     this.ngOnInit();
                 },
                 error => {
@@ -114,7 +78,6 @@ export class AdminComponent implements OnInit {
                     this.users = [];
                     this.approvedUsers = [];
                     this.disapprovedUsers = [];
-                    this.auctionJsonUri = [];
                     this.ngOnInit();
                 },
                 error => {
@@ -122,11 +85,41 @@ export class AdminComponent implements OnInit {
                 });
 	}
 
+	getXML(i:number) {
+		// var file:any;
+		// this.downloadXML(i, 'n').then(file => {
+		// 	console.log(file);
+		// });
+		// console.log(file);
+		// setTimeout(() => {
+		// 	const blob = new Blob([file], { type: 'text/xml' });
+		// 	console.log(blob);
+		// 	saveAs(blob, "auction.xml");
+		// }, 3000);
+		// this.file = null;
+		this.file = this.downloadXML(i);
+		setTimeout(() => {
+			const blob = new Blob([this.file], { type: 'text/xml' });
+			console.log(blob);
+			saveAs(blob, "auction.xml");
+			this.file = null;
+		}, 3000);	
+	}
+
+	getJSON(i: number) {
+		let xw = this.downloadXML(i);
+		var convert = require('xml-js');
+		var options = {compact: true, ignoreComment: true, spaces: 4};
+		var theJSON = convert.xml2json(xw, options);
+		setTimeout(() => {
+			const blob = new Blob([theJSON], { type: 'text/json' });
+			console.log(blob);
+			saveAs(blob, "auction.json");
+		}, 3000);
+	}
+
 	downloadXML(i: number) {
-		var link = document.createElement("a");
-		link.download = 'auction.xml';
 		var auct = this.auctions[i];
-		// console.log(auct.name);			
 		// XML
 		var XMLWriter = require('xml-writer');
 		var xw = new XMLWriter(true);
@@ -141,20 +134,49 @@ export class AdminComponent implements OnInit {
 			xw.writeElement('Buy_Price', "$"+auct.buy_price);
 		xw.writeElement('First_Bid', "$"+auct.first_bid);
 		xw.writeElement('Number_of_Bids', auct.number_of_bids);
-		// xw.startElement('Bids');
-		// xw.endElement();
 		xw.writeElement('Location', auct.location.name).writeAttribute('Latitude', auct.location.latitude).writeAttribute('Longtitude', auct.location.longitude);
 		xw.writeElement('Country', auct.country);
 		if(auct.started){
 			xw.writeElement('Started', auct.startingDate);				
 			xw.writeElement('Ends', auct.endingDate);
 		}
-		// xw.writeElement('Seller');
-		xw.writeElement('Description', auct.description);
-		let XMLfile:any;
-		XMLfile = this.sanitizer.bypassSecurityTrustUrl("data:application/xml;charset=UTF-8," + encodeURIComponent(xw));
-		link.href = XMLfile;
-		console.log(link.href);
-		link.click();
+		
+		this.dataService.getById(auct.seller_id).pipe(first()).subscribe(seller => {
+			xw.startElement('Seller');
+			xw.writeAttribute('Rating', seller.sellerRating);
+			xw.writeAttribute('UserID', seller.username);
+			xw.endElement();
+			xw.writeElement('Description', auct.description);
+			xw.startElement('Bids');
+			auct.bids.forEach(bid => {
+				this.dataService.getBid(bid).pipe(first()).subscribe(bid => {
+					this.dataService.getById(bid.bidder_id).pipe(first()).subscribe(bidder => {
+						console.log("bid");
+						xw.startElement('Bidder');
+						xw.writeAttribute('Rating', bidder.bidderRating);
+						xw.writeAttribute('UserID', bidder.username);
+						xw.writeElement('Location', bidder.address.city);
+						xw.writeElement('Country', bidder.address.country);
+						xw.endElement();
+						xw.writeElement('Time', bid.time);
+						xw.writeElement('Amount', bid.amount);
+					});				
+				});
+			});
+			setTimeout(() => {
+				xw.endElement();		
+				this.file = xw;				
+			}, 2000);
+		});
+	
+	}
+
+	downloadJSON(i: number) {
+		// JSON
+		let xw = this.downloadXML(i);
+		var convert = require('xml-js');
+		var options = {compact: true, ignoreComment: true, spaces: 4};
+		var theJSON = convert.xml2json(xw, options);
+		return theJSON;
 	}
 }
